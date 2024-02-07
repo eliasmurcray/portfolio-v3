e94543ecee8cfa5de6cebafadda5b4aa474b28d2
eliasmurcray <eliasmurcray@gmail.com>
2024-02-07 11:21:10 -0800

    Hero new fonts, increase right padding on thumbnails

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -14,20 +14,21 @@ class Hero extends React.Component {
 			item.style.animation = `slide-up 1s ${i * 80}ms ease-in-out forwards`;
 		});
 		
-		const ids = ["4588843984011264","4733225747136512","5332087555538944","5682299737522176","6415872009486336","6570069814460416","6649318067453952","5331465393717248","5474895617114112","5741521562550272","6458823309574144","6622069756837888"];
+		const ids = ["4588843984011264","4726917411422208","5332087555538944","5568335758540800","6041438572822528","4733225747136512","5388647456882688","5682299737522176","6458823309574144","6649318067453952","5331465393717248","5474895617114112","5741521562550272","6570069814460416"];
 		const slider = document.getElementById("thumbnail-slider") as HTMLDivElement;
+		const thumbnail_sizes = [100, 140, 150, 190];
 		ids.forEach((id) => {
 			let img = document.createElement("img");
 			img.src = id + ".png";
 			img.className = "project-thumbnail";
-			img.style.right = `${((Math.random() * 270) | 0) + 80}px`;
-			let s = [100, 140, 190][(Math.random() * 3) | 0];
-			let speed = [50, 70, 80][(Math.random() * 3) | 0] * 5;
+			img.style.right = `${((Math.random() * 250) | 0) + 100}px`;
+			let s = thumbnail_sizes[(Math.random() * thumbnail_sizes.length) | 0];
+			let speed = [35, 50, 80][(Math.random() * 3) | 0] * 4;
 			let curr = (Math.random() * -window.innerHeight - s + window.innerHeight) | 0;
 			const c = curr;
 			img.style.width = s + "px";
 			img.style.zIndex = s.toString();
-			img.style.filter = `blur(${[190, 140, 100].indexOf(s)}px)`;
+			img.style.filter = `blur(${thumbnail_sizes.slice().reverse().indexOf(s)}px)`;
 			img.style.top = curr + "px";
 			slider.append(img);
 
@@ -39,7 +40,7 @@ class Hero extends React.Component {
 					img.style.zIndex = s.toString();
 					img.style.filter = `blur(${[190, 140, 100].indexOf(s)}px)`;
 					img.style.top = curr + "px";
-					speed = [100, 120, 150][(Math.random() * 3) | 0] * 5;
+					speed = [100, 120, 150][(Math.random() * 3) | 0] * 4;
 					img.style.transition = "none";
 					void(slider.offsetHeight);
 					img.style.transition = "top 5s linear";
@@ -67,8 +68,8 @@ class Hero extends React.Component {
 				</nav>
 			</header>
 			<div className="content">
-				<h1>Hi There, I'm Elias!</h1>
-				<p>I write code for websites, robots, games, and more. I really like cats, Vim, and TypeScript.</p>
+				<h1>Elias Murcray <div className="sideways-text">Web Designer</div></h1>
+				<p>I'm a passionate web designer with a keen eye for detail and a love for crafting beautiful and functional digital experiences.</p>
 				<a href="/blog">Check out my blog</a>
 			</div>
 			<div id="thumbnail-slider"></div>
